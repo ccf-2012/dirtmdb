3ee7a90765ddfcad60a8c792664189f3211d9362
ccf2012 <crlf66@163.com>
2023-04-07 23:01:11 +0800

    use xml.etree

--- dirtmdb.py
+++ dirtmdb.py
@@ -2,10 +2,50 @@ import os
 import glob
 import re
 import argparse
+import xml.etree.ElementTree as ET
 
 
 def uselessFile(entryName):
     return entryName in ['@eaDir', '.DS_Store', '.@__thumb']
+
+
+def rename_dirs_with_tmdb(root_dir):
+    for index, dirname in enumerate(os.listdir(root_dir)):
+        if uselessFile(dirname):
+            continue
+
+        dirpath = os.path.join(root_dir, dirname)
+        if os.path.isdir(dirpath):
+            nfo_found = False
+            for subfile in os.listdir(dirpath):
+                if os.path.isdir(os.path.join(dirpath, subfile)):
+                    # print(f"{subfile} skip")
+                    continue
+
+                if subfile.endswith('.nfo'):
+                    nfo_path = os.path.join(dirpath, subfile)
+
+                    tree = ET.parse(nfo_path)
+                    root = tree.getroot()
+
+                    # 查找根节点中的 <tmdb> 标识
+                    tmdb_elem = root.find('tmdbid')
+                    if tmdb_elem is not None:
+                        nfo_found = True
+                        tmdb_id = tmdb_elem.text
+                        original_dirname = os.path.basename(dirpath)
+                        if not re.search(r'tmdb-\d+', original_dirname, re.I):
+                            new_dirname = f"{original_dirname} {{tmdb-{tmdb_id}}}"
+                            new_dirpath = os.path.join(
+                                os.path.dirname(dirpath), new_dirname)
+                            print(f"{index} : {original_dirname} ==> {new_dirname}")
+                            os.rename(dirpath, new_dirpath)
+                        else:
+                            print(f"{index} : {original_dirname} skip")
+                        break
+            if not nfo_found:
+                thedirname = os.path.basename(dirpath)
+                print(f"{index} : {thedirname} .nfo file not found")
 
 
 def rename_movie_dirs_with_tmdb(root_dir):
@@ -55,7 +95,7 @@ def loadArgs():
 
 def main():
     loadArgs()
-    rename_movie_dirs_with_tmdb(ARGS.dir)
+    rename_dirs_with_tmdb(ARGS.dir)
 
 
 if __name__ == '__main__':
