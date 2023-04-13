38cea6fc65d4296ba6ddaa2940a8d1eb879bf398
ccf2012 <crlf66@163.com>
2023-04-13 22:32:29 +0800

    imdb

--- README.md
+++ README.md
@@ -6,19 +6,28 @@
 ```
 python3 dirtmdb.py -h
 
-usage: dirtmdb.py [-h] dir
+usage: dirtmdb.py [-h] [-i] [-t] [-s SUCCESS_LIST] dir
 
 rename folder with tmdbid.
 
 positional arguments:
-  dir         folder path.
+  dir                   folder path.
 
 options:
-  -h, --help  show this help message and exit
+  -h, --help            show this help message and exit
+  -i, --imdb            rename folder with IMDb id.
+  -t, --tmdb            rename folder with TMDb id.
+  -s SUCCESS_LIST, --success-list SUCCESS_LIST
+                        output success list.
 ```
 
 ## 例子
 ```
-python3 dirtmdb.py  /gd/movie/other
+python3 dirtmdb.py  /gd/movie/other -s sus.txt
+```
+* 如果要改名为带 IMDb 后缀：
+```
+python3 dirtmdb.py  --imdb /gd/movie/other
+
 ```
 
--- dirtmdb.py
+++ dirtmdb.py
@@ -10,7 +10,7 @@ def uselessFile(entryName):
 
 
 def rename_dirs_with_tmdb(root_dir):
-    fo = open("sus.txt", "w", encoding='utf-8')
+    fo = open(ARGS.success_list, "w", encoding='utf-8')
     for index, dirname in enumerate(os.listdir(root_dir)):
         if uselessFile(dirname):
             continue
@@ -68,7 +68,8 @@ def rename_dirs_with_tmdb(root_dir):
     fo.close()
 
 
-def rename_movie_dirs_with_tmdb(root_dir):
+def rename_dirs_with_imdb(root_dir):
+    fo = open(ARGS.success_list, "w", encoding='utf-8')
     for index, dirname in enumerate(os.listdir(root_dir)):
         if uselessFile(dirname):
             continue
@@ -83,39 +84,67 @@ def rename_movie_dirs_with_tmdb(root_dir):
 
                 if subfile.endswith('.nfo'):
                     nfo_path = os.path.join(dirpath, subfile)
-                    with open(nfo_path, 'r', encoding='utf-8') as f:
-                        nfo_content = f.read()
-                    match = re.search(
-                        r'<uniqueid.*type="tmdb".*>([0-9]+)</uniqueid>', nfo_content)
-                    if match:
+                    if not os.path.isfile(nfo_path):
+                        continue
+                    
+                    imdb_elem = None
+                    try:
+                        tree = ET.parse(nfo_path)
+                        root = tree.getroot()
+                        # 查找根节点中的 <tmdbid> 标识
+                        imdb_elem = root.find('imdbid')
+                    except:
+                        continue
+
+                    if imdb_elem is not None and imdb_elem.text is not None:
                         nfo_found = True
-                        tmdb_id = match.group(1)
+                        tmdb_id = imdb_elem.text
                         original_dirname = os.path.basename(dirpath)
-                        if not re.search(r'tmdb-\d+', original_dirname, re.I):
-                            new_dirname = f"{original_dirname} {{tmdb-{tmdb_id}}}"
-                            new_dirpath = os.path.join(
-                                os.path.dirname(dirpath), new_dirname)
+                        m = re.search(r'imdb-tt\d+', original_dirname, re.I)
+                        if not m:
+                            new_dirname = f"{original_dirname} {{imdb-{tmdb_id}}}"
+                            new_dirpath = os.path.join( os.path.dirname(dirpath), new_dirname)
                             print(f"{index} : {original_dirname} ==> {new_dirname}")
-                            os.rename(dirpath, new_dirpath)
+                            # os.rename(dirpath, new_dirpath)
                         else:
-                            print(f"{index} : {original_dirname} skip")
+                            # print(f"{index} : {original_dirname} origin TMDb name.")
+                            new_dirname =  original_dirname.replace(m.group(0), f"imdb-{tmdb_id}")
+                            new_dirpath = os.path.join( os.path.dirname(dirpath), new_dirname)
+                            print(f"{index} : {original_dirname} ==> {new_dirname}")
+                        if os.path.exists(new_dirpath):
+                            print(f"{new_dirpath} exists, skip")
+                            break
+                        try:
+                            os.rename(dirpath, new_dirpath)
+                        except:
+                            print("rename exception")
+                        fo.write( new_dirname + os.linesep )
+
                         break
             if not nfo_found:
                 thedirname = os.path.basename(dirpath)
                 print(f"{index} : {thedirname} .nfo file not found")
+    fo.close()
+
 
 
 def loadArgs():
     global ARGS
     parser = argparse.ArgumentParser(description='rename folder with tmdbid.')
     parser.add_argument('dir', help='folder path.')
+    parser.add_argument('-i', '--imdb', action='store_true', help='rename folder with IMDb id.')
+    parser.add_argument('-t', '--tmdb', action='store_true', help='rename folder with TMDb id.')
+    parser.add_argument('-s', '--success-list',  help='output success list to a file.')
     ARGS = parser.parse_args()
     ARGS.dir = os.path.expanduser(ARGS.dir)
 
 
 def main():
     loadArgs()
-    rename_dirs_with_tmdb(ARGS.dir)
+    if ARGS.imdb:
+        rename_dirs_with_imdb(ARGS.dir)
+    else:
+        rename_dirs_with_tmdb(ARGS.dir)
 
 
 if __name__ == '__main__':
